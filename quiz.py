c64ee67be180d4b9d8575c385fc8246eea6d998a
Miska Savenius <miska.savenius@gmail.com>
2025-01-14 10:41:13 +0200

    Tietovisa tehty valmiiksi

--- quiz.py
+++ quiz.py
@@ -1,35 +1,32 @@
+import json
 def quiz():
-    def quiz():
-
     print("Welcome!")
-    play = input("Do you want to play a quiz? (yes/no): ").strip().lower()
-
-    if play != "yes":
-        print("Quitting. Goodbye!")
-        return
-
-    print("Ok, let´s start!")
-
     while True:
+        play = input("Do you want to play a quiz? (yes/no): ").strip().lower()
+
+        if play != "yes":
+            print("Quitting. Goodbye!")
+            break
+
+        print("Ok, let´s start!")
+
         with open('quiz.json', 'r') as tiedosto:
-        sisalto = tiedosto.read()
-        correct_ones = 0
-
-        print(sisalto.kysymys)
-        answer1 = int(input("What is? "))
-        if answer1 == 2:
-            correct_ones = correct_ones + 1
-        print(sisalto.kysymys)
-        answer2 = int(input("What is? "))
-        if answer2 == 2:
-            correct_ones = correct_ones + 1
-        print(sisalto.kysymys)
-        answer3 = int(input("What is? "))
-        if answer3 == 2:
-            correct_ones = correct_ones + 1
-
-        print(f"You had {correct_ones} right answers out of 3. Goodbye!")
-        break
+            sisalto = json.load(tiedosto)
+            correct_ones = 0
+            q_num = 1
+            for question in sisalto:
+                print(f"Question {q_num}: {question['kysymys']}")
+                print(f"   1) {question['vaihtoehto1']}")
+                print(f"   2) {question['vaihtoehto2']}")
+                print(f"   3) {question['vaihtoehto3']}")
+                answer = int(input("Answer: "))
+                if answer == question["oikea_vastaus"]:
+                    correct_ones = correct_ones + 1
+                    print("Correct!")
+                else:
+                    print(f"Wrong! The correct answer was {question['oikea_vastaus']}")
+                print()
+            print(f"You had {correct_ones} right answers out of 3. Goodbye!")
 
 if __name__ == "__main__":
     quiz()
